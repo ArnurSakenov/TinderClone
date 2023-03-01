2a8fd63ae10244c40a9850b2285f20de9bd6ee8f
ArnurSakenov <arnur.s@mail.ru>
2023-03-01 18:59:12 +0600

    Initial Commit

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Arnur Sakenov on 01.03.2023.
+//
+
+import UIKit
+import SnapKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
